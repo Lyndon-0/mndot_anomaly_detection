5ccc8e15f74c2aff493e5b6db5388d06f6779342
Denver Noell <denvernoell@gmail.com>
2023-05-12 13:38:32 -0700

    Formatting changes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from pathlib import Path
 import fiona
 
+st.set_page_config(layout="wide", page_title="Frick Unit Map", page_icon="💧",)
 
 P = Path(r"data")
 @st.cache_data
@@ -13,93 +14,67 @@ def load_files():
 	gdbs = {file.stem:{layer:gpd.read_file(file,driver='FileGDB',layer=layer) for layer in fiona.listlayers(file)} for file in P.glob('**/*.gdb')}
 	return gdbs
 
-
+st.markdown("# Frick Unit Map")
 class Map:
 	def __init__(self):
 		self.map = leafmap.Map(
 			google_map="HYBRID",
-			# draw_control=False,
-			draw_control=True,
+			draw_control=False,
+			# draw_control=True,
 		)
 
-	# def add_boundaries(self,boundaries):
-	# 	self.map.add_gdf(boundaries,layer_name="Boundaries",info_mode='on_click',)
-
-	# def add_markers_and_labels(self,gdf,label_col,color,icon,prefix='glyphicon',hover_cols='all',name=None):
-		
-	# 	for i,row in gdf.iterrows():
-	# 		# st.dataframe(row['COUNTY'])
-	# 		# st.write(type(row.to_frame()))
-	# 		# st.markdown(type(row))
-	# 		row['Point Type'] = name
-
-	# 		frame=row.to_frame()
-	# 		# make point type first column
-	# 			# frame = frame.reindex(['Point Type'] + frame.columns[:-1].tolist(), axis=1)
-
-	# 		popup = frame.to_html(
-	# 			header=False,
-	# 			escape=False,
-	# 			render_links=True,
-	# 			# col_space=10,
-	# 			border=3,
-	# 			)
-	# 		if hover_cols == 'all':
-	# 			tooltip = popup
-	# 		else:
-	# 			# add point type to hover_cols if not there
-	# 			if 'Point Type' not in hover_cols:
-	# 				hover_cols.insert(0,'Point Type')
-				
-				
-	# 			# st.dataframe(row.to_frame())
-	# 			tooltip = frame.loc[hover_cols].to_html(
-	# 				header=False,
-	# 				escape=False,
-	# 				render_links=True,
-	# 				# col_space=10,
-	# 				border=3,
-	# 				)
-
-			
-	# 		self.map.add_marker(
-	# 			(row.geometry.y,row.geometry.x),
-	# 			# popup=y['point_id'],
-	# 			# popup=popup,
-	# 			# tooltip=row[label_col],
-	# 			# tooltip=row.to_html(),
-	# 			# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_html.html
-	# 			popup=popup,
-	# 			tooltip=tooltip,
-	# 			draggable=True,
-	# 			# tooltip=f"{row.iloc[:].to_dict() }",
-	# 			icon=folium.Icon(
-	# 			color=color,
-	# 			icon=icon,
-	# 			prefix=prefix,
-	# 			label=label_col,
-	# 			# icon='bi bi-droplet'
-	# 			),
-	# 			# tooltip
-	# 			)
-	# 	gdf['latitude'] = gdf.geometry.y
-	# 	gdf['longitude'] = gdf.geometry.x
-	# 	self.map.add_labels(
-	# 		data=gdf,
-	# 		x='longitude',
-	# 		y='latitude',
-	# 		column=label_col,
-	# 		font_color='white',
-	# 		# font_color='black',
-
-	# 		font_size='12pt',
-	# 		layer_name='Wells',
-	# 		# label_col=label_col,
-	# 	)
 M = Map()
 gdbs = load_files()
 
+
+layer_rules = {
+	"Block Reference 9194F":{
+		# 'Polygons':{"color":"red","fill_color":"red","fill_opacity":0.5,"weight":1,"opacity":0.5},
+		'Polylines':{"color":"red","opacity":1,"weight":15,'label':False},
+	},
+	"Frick Unit Areas":{
+		'Polygons':{"color":"orange","fill_color":"blue","fill_opacity":0.5,"weight":8,"opacity":0.5,'label':True,'font_size':12},
+	},
+	"Frick Unit":{
+		'Points':{"color":"purple",'label':True,'font_size':8},
+		'Polylines':{"color":"pink","weight":8,'label':False},
+		'Polygons':{"color":"green","fill_color":"green","weight":10,"fill":False,"opacity":0.5,'label':False},
+	},
+}
+
+
+
+
 for gdb in gdbs:
 	for layer in gdbs[gdb]:
-		M.map.add_gdf(gdbs[gdb][layer],layer_name=f"{gdb}: {layer}",info_mode='on_click',)
+		gdf = gdbs[gdb][layer]
+		options = layer_rules[gdb][layer]
+		M.map.add_gdf(
+		gdf,
+		layer_name=f"{gdb}: {layer}",
+		info_mode='on_click',
+		style=options,
+		)
+		if options['label'] == True:
+			M.map.add_labels(
+				data=gdf,
+				column='Name',
+				# font_color='white',
+				font_color=options['color'],
+				font_size=f'{options["font_size"]}pt',
+
+			)
+
+
+
+legend_dict = {f"{gdf}: {k}":v['color'] for gdf,layer in layer_rules.items() for k,v in layer.items()}
+# st.markdown(legend_dict)
+
+M.map.add_legend(
+	# builtin_legend='NLCD',
+	title='Legend',
+	legend_dict=legend_dict,
+	)
+
+
 M.map.to_streamlit()

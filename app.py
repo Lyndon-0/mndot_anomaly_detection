2d8132bade9578288ace230c3e613f6be4403d0a
Denver Noell <denvernoell@gmail.com>
2023-07-17 10:18:30 -0700

    visual changes

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from data import gdf,apns
 st.set_page_config(layout="wide", page_title="Frick Unit Map", page_icon="💧",)
 st.title("Frick Unit Map")
 from map_utils import make_map
+
+
+# st.dataframe(apns.drop(columns=['geometry']))
 m = make_map(gdf,apns)
 m.to_streamlit()
 st.markdown("Note: Turnout locations and pipeline sizes are subject to change depending upon input received from landowners")
-
--- map_utils.py
+++ map_utils.py
@@ -1,15 +1,27 @@
 import folium
 import leafmap.foliumap as leafmap
+import streamlit as st
 def make_map(full_gdf,apns):
 	lines = ['Proposed Pipeline', 'AEWSD North Canal', 'FFPPP Discharge Pipeline',"AEWSD Alignments"]
 	filled_polygons = ['Frick Unit Service Area']
 	hollow_polygons = ['District Boundary']
 	points = ['Proposed Turnout']
 
-	clip_layers = ['Proposed Turnout', 'Proposed Pipeline',"Frick Unit Service Area","AEWSD Alignments"]
+	clip_layers = [
+		'Proposed Turnout',
+		# 'Proposed Pipeline',
+		"Frick Unit Service Area",
+		# "AEWSD Alignments",
+		]
 	service_boundary = full_gdf.pipe(lambda df:df.loc[df['label'] == "Frick Unit North Service Area"])
+	intersect = lambda gdf: gdf[gdf.intersects(service_boundary.unary_union)]
 	clip = lambda gdf: gdf.clip(service_boundary)
-		
+	apns = apns.to_crs("EPSG:4326")
+	# find interescting shapes
+	# intersecting_apns = apns[apns.intersects(service_boundary.unary_union)]
+
+
+
 	m = leafmap.Map(
 		# tiles="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
 		# attr="Esri",
@@ -30,120 +42,128 @@ def make_map(full_gdf,apns):
 	)
 
 	for layer in hollow_polygons:
-		gdf = full_gdf[full_gdf['layer']==layer]
-		if layer in clip_layers:
-			gdf = clip(gdf)
-		color = gdf['color'].unique()[0]
-		m.add_gdf(
-			gdf,
-			layer_name=layer,
-						# style_function=lambda x: {"color": "red", "fillOpacity": 0},
-			fields=['layer'],
-			highlight_function=lambda x: {"fillOpacity": 0.7, "weight": 6, "color": "lightgreen"},
-			style={
-				'color':color,
-				'fillColor':"none",
-				},
-			# highlight_function=lambda x: x['properties'],
-			# highlight_function=lambda x: {"l":x['properties']['label']},
-			# highlight_function=lambda x: {"l":x['properties']['label']},
-			)
+		try:
+			gdf = full_gdf[full_gdf['layer']==layer]
+			if layer in clip_layers:
+				gdf = clip(gdf)
+				# gdf = intersect(gdf)
+			color = gdf['color'].unique()[0]
+			m.add_gdf(
+				gdf,
+				layer_name=layer,
+							# style_function=lambda x: {"color": "red", "fillOpacity": 0},
+				fields=['layer'],
+				highlight_function=lambda x: {"fillOpacity": 0.7, "weight": 6, "color": "lightgreen"},
+				style={
+					'color':color,
+					'fillColor':"none",
+					},
+				# highlight_function=lambda x: x['properties'],
+				# highlight_function=lambda x: {"l":x['properties']['label']},
+				# highlight_function=lambda x: {"l":x['properties']['label']},
+				)
+		except:
+			pass
+			# st.markdown(f"Error with {layer}")
+			# print(f"Error with {layer}")
 
 	for layer in filled_polygons:
-		gdf = full_gdf[full_gdf['layer']==layer]
-		if layer in clip_layers:
-			gdf = clip(gdf)
-		color = gdf['color'].unique()[0]
-		m.add_gdf(
-			gdf,
-			layer_name=layer,
-			# highlight_function=lambda x: {"l":x['properties']['label']},
-			fields=['layer'],
-			highlight_function=lambda x: {"fillOpacity": 0.4, "weight": 6, "color": "lightgreen"},
-			style={
-				# 'color':color,
-				'color':"none",
-				'fillColor':color,
-				"tooltip":"label",
+		try:
 				
+			gdf = full_gdf[full_gdf['layer']==layer]
+			if layer in clip_layers:
+				gdf = clip(gdf)
+				# gdf = intersect(gdf)
+			color = gdf['color'].unique()[0]
+			m.add_gdf(
+				gdf,
+				layer_name=layer,
+				# highlight_function=lambda x: {"l":x['properties']['label']},
+				fields=['layer'],
+				highlight_function=lambda x: {"fillOpacity": 0.4, "weight": 6, "color": "lightgreen"},
+				style={
+					# 'color':color,
+					'color':"none",
+					'fillColor':color,
+					"tooltip":"label",
+					},
+					# tooltip="label",
+				zoom_to_layer=True,
+				)
 
-				},
-				# tooltip="label",
-			zoom_to_layer=True,
-			)
+		except:
+			st.markdown(f"Error with {layer}")
+			# print(f"Error with {layer}")
+	
+	intersecting_apns = apns[apns.intersects(service_boundary.unary_union)]
+	# st.dataframe(intersecting_apns.drop(columns=['geometry']))
+	apn_gdf = intersecting_apns#)
 
-	for layer in lines:
-		gdf = full_gdf[full_gdf['layer']==layer]
-		if layer in clip_layers:
-			gdf = clip(gdf)
-		color = gdf['color'].unique()[0]
-		m.add_gdf(
-			gdf,
-			layer_name=layer,
-			fields=['layer'],
-			highlight_function=lambda x: {"fillOpacity": 0.7, "weight": 6, "color": "lightgreen"},
-			# tooltip="label",
-			style={'color': color},
-			)
 
-	for layer in points:
-		gdf = full_gdf[full_gdf['layer']==layer]
-		if layer in clip_layers:
-			gdf = clip(gdf)
-		color = gdf['color'].unique()[0]
-		for i,y in gdf.iterrows():
-			folium.Circle(
-				radius=30,
-				location=[y.geometry.y,y.geometry.x],
-				color=y['color'],
-				fill=True,
-				# tooltip=[y["label"]],
-				tooltip="Proposed Turnout",
-				).add_to(m)
-	# add apns and labels
-	apn_gdf = clip(apns)
+	apn_gdf['size'] = apn_gdf.geometry.area  
+	# apn_gdf = apns
+	# st.dataframe(apn_gdf.drop(columns=['geometry']))
 	m.add_gdf(
 		apn_gdf,
 		# layer_name="APNs",
 		# style_function=lambda x: {"color": "red", "fillOpacity": 0},
-		fields=['APN_DASH'],
+		fields=['APN','Acreage','Landowner'],
 		# highlight_function=lambda x: {"fillOpacity": 0.7, "weight": 6, "color": "lightgreen"},
+		
 		style={
 			'color':"white",
 			"weight":0.5,
 		},
 	)
-	# import streamlit as st
-	# label_size = st.slider("Label Size",min_value=1,max_value=20,value=7)
-	# # st.markdown()
-	# m.add_labels(
-	# 	apn_gdf,
-	# 	# layer_name="APNs",
-	# 	column="APN_DASH",
-	# 	font_color="white",
-	# 	# font_size="7pt",
-	# 	font_size=f"{label_size}pt",
-	# 	# font_family="courier new",
-	# 	# font_weight="bold",
-	# 	draggable=False,
-	# )
-	# draw a leader line to the centroid of each apn
+	for layer in points:
+		try:
+			gdf = full_gdf[full_gdf['layer']==layer]
+			if layer in clip_layers:
+				gdf = clip(gdf)
+				# intersect(gdf)
+			color = gdf['color'].unique()[0]
+			for i,y in gdf.iterrows():
+				folium.Circle(
+					radius=30,
+					location=[y.geometry.y,y.geometry.x],
+					color=y['color'],
+					fill=True,
+					# tooltip=[y["label"]],
+					tooltip="Proposed Turnout",
+					).add_to(m)
+			# add apns and labels
+			
+		except:
+			st.markdown(f"Error with {layer}")
 
 
+	for layer in lines:
+		try:
+			gdf = full_gdf[full_gdf['layer']==layer]
+			# st.markdown(layer)
+			# st.markdown(gdf.crs)
+			if layer in clip_layers:
+				gdf = clip(gdf)
+				# intersect(gdf)
+			color = gdf['color'].unique()[0]
+			m.add_gdf(
+				gdf,
+				layer_name=layer,
+				fields=['layer','label'],
+				highlight_function=lambda x: {"fillOpacity": 0.7, "weight": 6, "color": "lightgreen"},
+				# tooltip="label",
+				style={'color': color},
+				)
 
-	# for i,y in apn_gdf.iterrows():
-	# 	folium.Marker(
-	# 		location=[y.geometry.centroid.y,y.geometry.centroid.x],
-	# 		icon=folium.DivIcon(html=f"""
-	# 	       <div style="
-	# 	       font-family: courier new;
-	# 	       color: white;
-	# 	       font-size: 12pt;
-	# 	    #    background-color: #ff0000;
-	# 	    #    border: 1px solid red;
-	# 	       ">{y['APN_DASH']}</div>""")
-	# 		# icon=folium.DivIcon(html=f"""<div style="font-family: courier new; color: black">{y['APN_DASH']}</div>""")
-	# 		).add_to(m)
+		except:
+			st.markdown(f"Error with {layer}")
+	# apns.crs = "EPSG:4326"
+	# st.markdown(apns.crs)
+	# apn_gdf = clip(apns.to_crs("EPSG:4326"))
+	# apn_gdf = clip(apns)
+	# apn_gdf = apns.to_crs("EPSG:4326")
+
+
 	m.add_legend(
 		title="Legend",
 		legend_dict={
@@ -157,12 +177,7 @@ def make_map(full_gdf,apns):
 			"APN": "white",
 		}
 	)
-
-
+	m.zoom_to_gdf(apn_gdf)
 	# m.fit_bounds(bounds=ll_bounds,max_zoom=12)
 	# m.set_max_bounds(ll_bounds)
 	return m
-# filtered_gdf = gdf.clip(bounds)
-# filtered_gdf = gdf.clip(bounds)
-# m = make_map(gdf)
-# m.to_streamlit()

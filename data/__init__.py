9f8a8048f7780ae42a29a120cf4e0a3644c7c914
Denver Noell <denvernoell@gmail.com>
2023-08-15 10:09:48 -0700

    Added new areas

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 from pathlib import Path
+import pandas as pd
+
 data_path = Path("data")
+config = pd.read_excel(data_path.joinpath('config.xlsx'), sheet_name='layers')
 gdf = gpd.read_parquet(data_path.joinpath("gdf.parquet"))
 apns = gpd.read_parquet(data_path.joinpath("apns.parquet"))
 pipes = gpd.read_parquet(data_path.joinpath("pipes.parquet"))

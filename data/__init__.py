b608dce792d08a021bf8f4aae3db1c0be316216b
Denver Noell <denvernoell@gmail.com>
2023-08-23 10:11:33 -0700

    Changed apn file

--- data/__init__.py
+++ data/__init__.py
@@ -9,17 +9,6 @@ config = pd.read_excel(data_path.joinpath('config.xlsx'), sheet_name='layers')
 # df['layer'] == 'Panama Unit Service Area' & df['label'] == 'Frick Unit North Service Area'
 # df['layer'] == 'Frick Unit Pipeline' & df['label'] == 'Frick Unit'
 gdf = gpd.read_parquet(data_path.joinpath("gdf-2023-08-15.parquet"))
-# .pipe(
-# 	lambda df: df.loc[
-# 		(df['layer'] == 'Panama Unit Service Area') &
-# 		(df['label'] != 'Frick Unit North Service Area')
-# 	]
-# ).pipe(
-# 	lambda df: df.loc[
-# 		(df['layer'] == 'Frick Unit Pipeline')&
-# 		(df['label'] != 'Frick Unit')
-# 	]
-# )
 district_boundary = gdf.pipe(lambda df:df.loc[df['layer'] == "District Boundary"])
 # apns = gpd.read_parquet(data_path.joinpath("apns.parquet")).to_crs("EPSG:4326")
 # apn_gdf = apns[apns.intersects(district_boundary.unary_union)]

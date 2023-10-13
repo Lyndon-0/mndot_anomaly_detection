14ac6bb65dd2360a9aabc848264fa21524bcbf4a
Denver Noell <denvernoell@gmail.com>
2023-10-13 14:50:06 -0700

    updated parquet

--- data/__init__.py
+++ data/__init__.py
@@ -8,7 +8,8 @@ config = pd.read_excel(data_path.joinpath('config.xlsx'), sheet_name='layers')
 # filter out the following
 # df['layer'] == 'Panama Unit Service Area' & df['label'] == 'Frick Unit North Service Area'
 # df['layer'] == 'Frick Unit Pipeline' & df['label'] == 'Frick Unit'
-gdf = gpd.read_parquet(data_path.joinpath("gdf-2023-08-15.parquet"))
+# gdf = gpd.read_parquet(data_path.joinpath("gdf-2023-08-15.parquet"))
+gdf = gpd.read_parquet(data_path.joinpath("gdf-2023-10-13.parquet"))
 district_boundary = gdf.pipe(lambda df:df.loc[df['layer'] == "District Boundary"])
 # apns = gpd.read_parquet(data_path.joinpath("apns.parquet")).to_crs("EPSG:4326")
 # apn_gdf = apns[apns.intersects(district_boundary.unary_union)]

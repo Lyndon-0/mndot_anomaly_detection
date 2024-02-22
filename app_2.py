0fb6d819d561704b35ce2bbb341cf37b0280ff75
Denver Noell <denvernoell@gmail.com>
2024-02-22 10:39:47 -0800

    Updated to merge

--- app_2.py
+++ app_2.py
@@ -1,20 +1,14 @@
 import streamlit as st
 from pathlib import Path
 
-# from data import gdf,apn_gdf,pipes,config
-# from util.simple_map_utils import make_map
+from data import gdf,apn_gdf,pipes,config
+from util.full_map_utils import make_map
 st.set_page_config(layout="wide", page_title="Frick Unit Map", page_icon="💧",)
 st.title("Frick Unit Map")
 
-from data import get_config, get_gdf
-from util.map_utils import plot_map
-# from data import get_config, get_gdf
-sheet_name = "Full"
-gdf = get_gdf(sheet_name)
-config = get_config(sheet_name)
 
 # st.dataframe(apns.drop(columns=['geometry']))
-m = plot_map(gdf, config)
+m = make_map(gdf,apn_gdf,pipes,config)
 m.to_streamlit(
 	height=800,
 )
